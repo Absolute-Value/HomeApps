8aac824c9da498ea9fc1535499ec82d4a50aed22
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2025-06-21 15:59:02 +0000

    品名、画像を表示

--- app/pages/summary.py
+++ app/pages/summary.py
@@ -3,18 +3,33 @@ import sqlite3
 import pandas as pd
 import streamlit as st
 
+IMAGES_DIR = "/data/done"
 DB_PATH = "/data/expenses.db"
 
 def main():
     st.title("集計ページ")
     
     st.write("ここでは、登録されたレシートの集計結果を表示します。")
+    if st.button("更新"):
+        st.rerun()
 
     if os.path.exists(DB_PATH):
         conn = sqlite3.connect(DB_PATH)
-        df = pd.read_sql_query("SELECT * FROM invoices", conn)
-        st.dataframe(df)
+        invoice_df = pd.read_sql_query("SELECT * FROM invoices", conn)
+        st.dataframe(invoice_df, hide_index=True)
         conn.close()
+
+        selected_id = st.selectbox("IDを選択してください", invoice_df["id"])
+            
+        conn = sqlite3.connect(DB_PATH)
+        item_df = pd.read_sql_query(f"SELECT 品名, 金額, 単位 FROM items WHERE invoice_id = {selected_id}", conn)
+        st.dataframe(item_df, hide_index=True)
+        conn.close()
+
+        image_name = invoice_df.loc[invoice_df["id"] == selected_id, "画像名"].values[0]
+        image_path = os.path.join(IMAGES_DIR, image_name)
+        if os.path.exists(image_path):
+            st.image(image_path)
 
 if __name__ == "__main__":
     main()
