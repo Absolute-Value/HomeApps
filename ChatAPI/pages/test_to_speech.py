318631b905598a48e0ce91fcc4982ea184923d60
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2025-08-29 00:49:20 +0900

    TextToSpeechを実装

---
+++ ChatAPI/pages/test_to_speech.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from groq import Groq
+
+PAGE_TITLE = "Text To Speech"
+st.set_page_config(
+    page_title=PAGE_TITLE,
+    page_icon=":material/record_voice_over:",
+    initial_sidebar_state="expanded",
+    layout="wide",
+)
+st.title(PAGE_TITLE)
+
+client = Groq()
+MODEL_OPTIONS = ("playai-tts", "playai-tts-arabic")
+VOICE_OPTIONS = ("Arista-PlayAI", "Atlas-PlayAI", "Basil-PlayAI", "Briggs-PlayAI", "Calum-PlayAI", "Celeste-PlayAI", "Cheyenne-PlayAI", "Chip-PlayAI", "Cillian-PlayAI", "Deedee-PlayAI", "Fritz-PlayAI", "Gail-PlayAI", "Indigo-PlayAI", "Mamaw-PlayAI", "Mason-PlayAI", "Mikail-PlayAI", "Mitch-PlayAI", "Quinn-PlayAI", "Thunder-PlayAI")
+ARABIC_VOICE_OPTIONS = ("Ahmad-PlayAI", "Amira-PlayAI", "Khalid-PlayAI", "Nasser-PlayAI")
+
+model = st.radio("model", MODEL_OPTIONS)
+if model == "playai-tts":
+    voices = VOICE_OPTIONS
+else:
+    voices = ARABIC_VOICE_OPTIONS
+voice = st.selectbox('voice', voices)
+text = st.text_area('text', value='I love building and shipping new features for our users!')
+response_format = "wav"
+
+response = client.audio.speech.create(
+    model=model,
+    voice=voice,
+    input=text,
+    response_format=response_format
+)
+
+audio_bytes = b"".join(response.iter_bytes())
+st.audio(audio_bytes, format="audio/wav")
