e47b7aab4049b376fabd5e913013ea51444ea1ac
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2025-08-31 01:01:25 +0900

    翻訳をformに変更

--- ChatAPI/task/translate.py
+++ ChatAPI/task/translate.py
@@ -4,7 +4,7 @@ from groq import Groq
 PAGE_TITLE = "翻訳"
 st.set_page_config(
     page_title=PAGE_TITLE,
-    page_icon=":material/chat:",
+    page_icon=":material/translate:",
     initial_sidebar_state="expanded",
     layout="wide",
 )
@@ -12,40 +12,26 @@ st.title(PAGE_TITLE)
 
 client = Groq()
 
-LANGUAGE_OPTIONS = ('日本語', '英語')
-if "before_lang" not in st.session_state:
-    st.session_state.before_lang = LANGUAGE_OPTIONS[1]
-if "after_lang" not in st.session_state:
-    st.session_state.after_lang = LANGUAGE_OPTIONS[0]
-
-@st.fragment
-def language_fragment():
-    language_container = st.container(horizontal=True, vertical_alignment="center")
-    st.session_state.before_lang = language_container.text_input(
-        "before", value=st.session_state.before_lang, placeholder="翻訳前", 
-        autocomplete="language", width=150, label_visibility="collapsed"
+with st.form("my_form"):
+    container = st.container(horizontal=True, vertical_alignment="center")
+    container.text("翻訳先：")
+    language = container.text_input(
+        "翻訳先", value="日本語", placeholder="翻訳先", 
+        autocomplete="language", label_visibility="collapsed"
     )
-    language_container.text('から')
-    st.session_state.after_lang = language_container.text_input(
-        "after", value=st.session_state.after_lang, placeholder="翻訳後", 
-        autocomplete="language", width=150, label_visibility="collapsed"
+    user_input = st.text_area(
+        '翻訳したい文章', height="content", 
+        placeholder="翻訳したい文章を入力してください", label_visibility="collapsed"
     )
-    if language_container.button("<->"):
-        tmp = st.session_state.before_lang
-        st.session_state.before_lang = st.session_state.after_lang
-        st.session_state.after_lang = tmp
-        st.rerun(scope="fragment")
+    st.form_submit_button('翻訳')
 
-language_fragment()
-user_input = st.text_area('翻訳したい文章', height="content", placeholder="翻訳したい文章を入力してください", label_visibility="collapsed")
-
-if st.button('翻訳'):
+if user_input:
     response = client.chat.completions.create(
         model="moonshotai/kimi-k2-instruct",
         messages=[
             {
                 "role": "system",
-                "content": f"ユーザーから与えられた文章を{st.session_state.before_lang}から{st.session_state.after_lang}に翻訳してください。\n翻訳した文章のみを出力してください。"
+                "content": f"ユーザーから与えられた文章を{language}に翻訳してください。\n翻訳した文章のみを出力してください。"
             },
             {
                 "role": "user",
@@ -55,9 +41,12 @@ if st.button('翻訳'):
         temperature=0,
         stream=True,
     )
-    response_placeholder = st.empty()
+    message = st.chat_message("assistant", avatar=":material/translate:")
+    response_placeholder = message.empty()
     response_text = ""
     for chunk in response:
         if chunk.choices[0].finish_reason != 'stop':
             response_text += chunk.choices[0].delta.content
             response_placeholder.markdown(response_text)
+else:
+    st.write('文章が入力されていません。')
