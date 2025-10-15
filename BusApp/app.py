4c197a81096196da312b749858fefb01061724e4
Absolute-Value <jikuya@cv.info.gifu-u.ac.jp>
2025-10-16 00:29:25 +0900

    bus-appをFastAPIに

--- BusApp/app.py
+++ BusApp/app.py
@@ -1,8 +1,12 @@
 import requests
-import pandas as pd
-import streamlit as st
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 PAGE_TITLE = 'バス情報'
 BASE_URL = 'https://transfer-cloud.navitime.biz'
@@ -24,55 +28,15 @@ STOPS = {
 }
 stops_list = list(STOPS.keys())
 
-st.set_page_config(
-    page_title=PAGE_TITLE,
-    page_icon=':material/directions_bus:',
-    layout='wide'
-)
-st.title(PAGE_TITLE)
+# メモリ内にデータを保存するための辞書
+bus_data_cache = {}
+cache_date = None
 
-cur_time = datetime.now()
-today = cur_time.strftime("%Y-%m-%d")
-st.write(today)
-if "today" not in st.session_state:
-    st.session_state["today"] = today
-
-if 'indexes' not in st.session_state:
-    st.session_state['indexes'] = [1,0]
-
-@st.fragment
-def station_form():
-    container = st.container(horizontal=True, horizontal_alignment="left", vertical_alignment="center")
-    st.session_state['indexes'][0] = container.selectbox(
-        "出発",
-        list(range(len(stops_list))),
-        format_func=lambda option: stops_list[int(option)],
-        index=st.session_state['indexes'][0],
-        label_visibility="collapsed",
-        width=200
-    )
-    container.text('から')
-    st.session_state['indexes'][1] = container.selectbox(
-        "到着",
-        list(range(len(stops_list))),
-        format_func=lambda option: stops_list[int(option)],
-        index=st.session_state['indexes'][1],
-        label_visibility="collapsed",
-        width=200
-    )
-    if container.button("<->"):
-        st.session_state['indexes'].reverse()
-        st.rerun(scope="fragment")
-station_form()
-
-departure_busstop = stops_list[int(st.session_state['indexes'][0])]
-arrival_busstop = stops_list[int(st.session_state['indexes'][1])]
-from_to = f'{departure_busstop}_{arrival_busstop}'
-
-if st.button("検索") and departure_busstop != arrival_busstop:
+def get_approach_data(departure_stop, arrival_stop):
+    """接近情報を取得"""
     params = {
-        "departure-busstop": STOPS[departure_busstop],
-        "arrival-busstop": STOPS[arrival_busstop],
+        "departure-busstop": STOPS[departure_stop],
+        "arrival-busstop": STOPS[arrival_stop],
     }
     resp = requests.get(APPROACH_URL, params=params)
     soup = BeautifulSoup(resp.text, "html.parser")
@@ -81,32 +45,40 @@ if st.button("検索") and departure_busstop != arrival_busstop:
     bound_for_items = []
     pole_items = []
     pole_map_items = []
+    
     rows = soup.find_all("button", class_=lambda x: x and 'rounded' in x and 'drop-shadow-md' in x)
     for row in rows:
-        approach_items.append(row.find('div', class_='text-lg my-3 text-center font-bold text-error').get_text())
-        bound_for_items.append(row.find('span', class_='font-bold').get_text())
-        pole_items.append(row.find('dt').get_text().lstrip('のりば: '))
-        pole_map_items.append(row.find('a'))
+        approach_elem = row.find('div', class_='text-lg my-3 text-center font-bold text-error')
+        bound_for_elem = row.find('span', class_='font-bold')
+        pole_elem = row.find('dt')
+        map_elem = row.find('a')
+        
+        if approach_elem:
+            approach_items.append(approach_elem.get_text())
+        if bound_for_elem:
+            bound_for_items.append(bound_for_elem.get_text())
+        if pole_elem:
+            pole_items.append(pole_elem.get_text().lstrip('のりば: '))
+        if map_elem and map_elem.get('href'):
+            pole_map_items.append(BASE_URL + map_elem.get('href'))
+        else:
+            pole_map_items.append('')
     
-    approach_df = pd.DataFrame(
-        {
-            '接近情報': approach_items,
-            '系統・行先': bound_for_items,
-            'のりば': pole_items,
-            'のりばマップ': pole_map_items,
+    return list(zip(approach_items, bound_for_items, pole_items, pole_map_items))
+
+def get_bus_schedule(departure_stop, arrival_stop):
+    """バス時刻表を取得"""
+    global bus_data_cache, cache_date
+    
+    today = datetime.now().strftime("%Y-%m-%d")
+    from_to = f'{departure_stop}_{arrival_stop}'
+    
+    if from_to not in bus_data_cache or cache_date != today:
+        params = {
+            "departure-busstop": STOPS[departure_stop],
+            "arrival-busstop": STOPS[arrival_stop],
+            "date": today
         }
-    )
-
-    st.dataframe(
-        approach_df,
-        column_config={
-            'のりばマップ': st.column_config.LinkColumn('のりばマップ', display_text='マップ'),
-        },
-        hide_index=True
-    )
-
-    if from_to not in st.session_state or st.session_state["today"] != today:
-        params["date"] = today
         resp = requests.get(URL, params=params)
         soup = BeautifulSoup(resp.text, "html.parser")
 
@@ -115,34 +87,99 @@ if st.button("検索") and departure_busstop != arrival_busstop:
         pole_elems = soup.find_all("a", class_="mr-2 block")
         all_stop_elems = soup.find_all("a", class_="mr-4")
 
-        departure_times = [time_elem.find_all("time")[0].get_text(strip=True) for time_elem in time_elems]
-        arrival_times = [time_elem.find_all("time")[1].get_text(strip=True) for time_elem in time_elems]
-        durings = [time_elem.find("span", class_="text-text-grey").get_text(strip=True) for time_elem in time_elems]
-        ways = [way_elem.find("span", class_="font-bold").get_text(strip=True) for way_elem in way_elems]
-        pole_links = [BASE_URL + pole_elem["href"] for pole_elem in pole_elems]
-        pole_nums = [pole_elem.get_text(strip=True) for pole_elem in pole_elems]
-        all_stop_urls = [BASE_URL + all_stop_elem["href"] for all_stop_elem in all_stop_elems]
+        departure_times = []
+        arrival_times = []
+        durings = []
+        ways = []
+        pole_nums = []
+        all_stop_urls = []
+        
+        for i, time_elem in enumerate(time_elems):
+            time_tags = time_elem.find_all("time")
+            if len(time_tags) >= 2:
+                departure_times.append(time_tags[0].get_text(strip=True))
+                arrival_times.append(time_tags[1].get_text(strip=True))
+                
+            duration_elem = time_elem.find("span", class_="text-text-grey")
+            durings.append(duration_elem.get_text(strip=True) if duration_elem else '')
+            
+            if i < len(way_elems):
+                way_elem = way_elems[i].find("span", class_="font-bold")
+                ways.append(way_elem.get_text(strip=True) if way_elem else '')
+            
+            if i < len(pole_elems):
+                pole_nums.append(pole_elems[i].get_text(strip=True))
+                
+            if i < len(all_stop_elems):
+                all_stop_urls.append(BASE_URL + all_stop_elems[i]["href"])
 
-        st.session_state[from_to] = pd.DataFrame(
-            {
-                '出発時刻': departure_times,
-                '到着時刻': arrival_times,
-                '移動時間': durings,
-                '系統・行先': ways,
-                'のりば': pole_nums,
-                'url': all_stop_urls,
-            }
-        )
-        st.write('roloaded')
+        schedule_data = list(zip(departure_times, arrival_times, durings, ways, pole_nums, all_stop_urls))
+        
+        # 現在時刻から30分前以降のデータのみ保持
+        cur_time = datetime.now()
+        filtered_schedule = []
+        for dep_time, arr_time, during, way, pole, url in schedule_data:
+            try:
+                dep_datetime = datetime.strptime(f'{today} {dep_time}', '%Y-%m-%d %H:%M')
+                if dep_datetime > cur_time - timedelta(minutes=30):
+                    filtered_schedule.append((dep_time, arr_time, during, way, pole, url))
+            except ValueError:
+                continue
+        
+        bus_data_cache[from_to] = filtered_schedule
+        cache_date = today
+    
+    return bus_data_cache[from_to]
 
-    st.session_state[from_to]['出発時刻_datetime'] = pd.to_datetime(st.session_state[from_to]['出発時刻'].apply(lambda x: f'{today} {x}'))
-    st.session_state[from_to] = st.session_state[from_to][st.session_state[from_to]['出発時刻_datetime'] > cur_time - timedelta(minutes=30)]
-    st.session_state[from_to] = st.session_state[from_to].drop(columns=['出発時刻_datetime'])
-    st.dataframe(
-        st.session_state[from_to],
-        column_config={
-            'url': st.column_config.LinkColumn('経路', display_text='通過時刻表'),
-        },
-        hide_index=True,
-        height=500
-    )
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request, departure: int = 0, arrival: int = 1):
+    """メインページ"""
+    today = datetime.now().strftime("%Y-%m-%d")
+    
+    # デフォルトの表示（検索前）
+    template_data = {
+        "request": request,
+        "page_title": PAGE_TITLE,
+        "today": today,
+        "stops": stops_list,
+        "selected_departure": departure,
+        "selected_arrival": arrival
+    }
+    
+    # 検索パラメータがある場合はバス検索を実行
+    if request.url.query and departure != arrival:
+        departure_stop = stops_list[departure]
+        arrival_stop = stops_list[arrival]
+        
+        try:
+            # 接近情報取得
+            approach_data = get_approach_data(departure_stop, arrival_stop)
+            
+            # 時刻表データ取得
+            schedule_data = get_bus_schedule(departure_stop, arrival_stop)
+            
+            template_data.update({
+                "departure_stop": departure_stop,
+                "arrival_stop": arrival_stop,
+                "approach_data": approach_data,
+                "schedule_data": schedule_data
+            })
+        except Exception as e:
+            template_data["error"] = "データの取得に失敗しました"
+    
+    elif request.url.query and departure == arrival:
+        template_data["error"] = "出発地と到着地が同じです"
+    
+    return templates.TemplateResponse("index.html", template_data)
+
+@app.post("/search")
+async def search(
+    departure: int = Form(...),
+    arrival: int = Form(...)
+):
+    """バス検索 - ルートページにリダイレクト"""
+    return RedirectResponse(url=f"./?departure={departure}&arrival={arrival}", status_code=303)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8503)
